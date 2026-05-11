ef56ba69812fa0a3bdbfa38c5f4bbfc951677044
eliaccio <elia.ricciolo@gmail.com>
2026-05-11 18:16:56 +0200

    aggiunta get e set alla classe Prenotazioni

---
+++ src/main/java/Prenotazioni.java
@@ -0,0 +1,13 @@
+public class Prenotazioni {
+    private User userPrenotante;
+    private Proiezioni proiezionePrenotata;
+
+    //costruttore
+    public Prenotazioni(User userPrenotante, Proiezioni proiezionePrenotata){
+        this.userPrenotante=userPrenotante;
+        this.proiezionePrenotata=proiezionePrenotata;
+    }
+
+
+
+}
