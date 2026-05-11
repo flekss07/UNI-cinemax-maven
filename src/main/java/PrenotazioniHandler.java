6c608134436f2d8f1bad1b29fa905acac1808e2e
eliaccio <elia.ricciolo@gmail.com>
2026-05-11 18:26:24 +0200

    creazione della classe PrenotazioniHandler

---
+++ src/main/java/PrenotazioniHandler.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+
+public class PrenotazioniHandler {
+    private LinkedList<User> userList;
+    private LinkedList<User> proiezioniList;
+
+    //costruttore
+    public PrenotazioniHandler (LinkedList proiezioniList, LinkedList userList){
+        this.proiezioniList=proiezioniList;
+        this.userList=userList;
+    }
+
+    //metodo di aggiunta prenotazione
+    public void addPrenotazione(){
+        Scanner sc = new Scanner(System.in);
+    }
+}
