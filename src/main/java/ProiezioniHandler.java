0782d6ff962a3d3fa71ef5ea31ef6aae68797ba7
flekss07 <edopia007@gmail.com>
2026-05-25 12:01:09 +0200

    aggiunti filtri per ricerca proiezioni, da fare filtro per prezzo e aggiunta di posti disponibili

--- src/main/java/ProiezioniHandler.java
+++ src/main/java/ProiezioniHandler.java
@@ -1,6 +1,7 @@
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -52,10 +53,44 @@ public class ProiezioniHandler {
         }
         return foundProj;
     }
+
+    // metodo di filtro per autore del film
+    public LinkedList<Proiezioni> filtroAutore(LinkedList<Proiezioni> pList, String target){
+        LinkedList<Proiezioni> filteredList = new LinkedList<>();
+        for (Proiezioni p:filteredList){
+            if(p.getRegista().toLowerCase().trim().contains(target.toLowerCase().trim())) // se autore trovato
+                filteredList.add(p); // lo aggiunge alla linkedlist di oggetti trovati
+        }
+        return filteredList; // ritorna la lista o vuota se non trova nulla o con i valori filtrati
+    }
+
+    // metodo di filtro per titolo del film
+    public LinkedList<Proiezioni> filtroTitolo(LinkedList<Proiezioni> pList, String target){
+        LinkedList<Proiezioni> filteredList = new LinkedList<>();
+        for (Proiezioni p:filteredList){
+            if(p.getTitolo().toLowerCase().trim().contains(target.toLowerCase().trim())) // se titolo trovato
+                filteredList.add(p); // lo aggiunge alla linkedlist di oggetti trovati
+        }
+        return filteredList; // ritorna la lista o vuota se non trova nulla o con i valori filtrati
+    }
+
+    // metodo di filtro per genere del film
+    public LinkedList<Proiezioni> filtroGenere(LinkedList<Proiezioni> pList, Genres target){
+        LinkedList<Proiezioni> filteredList = new LinkedList<>();
+        for (Proiezioni p:filteredList){
+            if(p.getGeneri().equals(target)) // se genere trovato
+                filteredList.add(p); // lo aggiunge alla linkedlist di oggetti trovati
+        }
+        return filteredList; // ritorna la lista o vuota se non trova nulla o con i valori filtrati
+    }
+
+    // metodo di filtro per intervallo di date del film
+    public LinkedList<Proiezioni> filtroGenere(LinkedList<Proiezioni> pList, LocalDateTime start, LocalDateTime end){
+        LinkedList<Proiezioni> filteredList = new LinkedList<>();
+        for (Proiezioni p:filteredList){
+            if(p.getData().isBefore(end) && p.getData().isAfter(start) || p.getData().isEqual(start) || p.getData().isEqual(end))  // se interv date trovato
+                filteredList.add(p); // lo aggiunge alla linkedlist di oggetti trovati
+        }
+        return filteredList; // ritorna la lista o vuota se non trova nulla o con i valori filtrati
+    }
 }
-
-
-
-
-
-
